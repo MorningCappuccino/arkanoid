2b9d13aa388ebcb9336e6cdf894bd9da44cafc9d
MorningCappuccino <knockknockwakeup@gmail.com>
2024-09-03 21:38:19 +0300

    init commit
    basic game elements

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, transform.position.y, 0);
+    }
+}
